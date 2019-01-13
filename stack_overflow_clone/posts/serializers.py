55f9b6057eb7736155405b52588d5fad6359cbdc
ABHYUDAY PRATAP SINGH <abhyudaypratap@outlook.com>
2019-01-14 04:27:05 +0530

    chore: services for post creation

--- stack_overflow_clone/posts/serializers.py
+++ stack_overflow_clone/posts/serializers.py
@@ -7,7 +7,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Post
-        fields = ['id', "title", "body", "post_type", "owner", "accepted_answer",
+        fields = ["id", "title", "body", "post_type", "owner", "accepted_answer",
                   "parent", "score", "view_count", "last_editor", "last_edit_date",
                   "answer_count", "comment_count", "favorite_count", "tags"]
 
---
+++ stack_overflow_clone/posts/services.py
@@ -0,0 +1,28 @@
+# StackOverflow Stuff
+from callerbox_rest.people.models import Post
+
+
+def create_post(body, post_type, owner,
+                tags=None, parent=None, title=None):
+    """
+    Create a post if name and phone_number unique
+    """
+    post = Post.objects.create(
+        body=body,
+        post_type=post_type,
+        owner=owner,
+        tags=tags,
+        parent=parent,
+        title=title
+    )
+    return post
+
+
+def get_post_by_id(post_id):
+    """
+    Search user by id
+    """
+    post = Post.objects.filter(id=post_id).first()
+    if post:
+        return post
+    return None
